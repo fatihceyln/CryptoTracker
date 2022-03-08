637455fe16f5a75ebe392f027f218b01162cea93
fatihceyln <fatihcyln@icloud.com>
2022-03-08 14:14:51 +0300

    Added Grids for overview and additional details and data connections completed

--- Crypto/Core/Home/Detail/ViewModels/DetailViewModel.swift
+++ Crypto/Core/Home/Detail/ViewModels/DetailViewModel.swift
@@ -10,21 +10,90 @@ import Combine
 
 class DetailViewModel: ObservableObject {
     
-    private let coinDetailService: CoinDetailDataService
+    @Published var overviewStatistics: [Statistic] = []
+    @Published var additionalStatistics: [Statistic] = []
     
+    @Published var coin: Coin
+    private let coinDetailService: CoinDetailDataService
     private var cancellables = Set<AnyCancellable>()
     
     init(coin: Coin) {
+        self.coin = coin
         self.coinDetailService = CoinDetailDataService(coin: coin)
         addSubscribers()
     }
     
     private func addSubscribers() {
         coinDetailService.$coinDetails
-            .sink { returnedCoinDetails in
-                print("RECEIVED COIN DETAIL DATA")
-                print(returnedCoinDetails)
+            .combineLatest($coin)
+            .map(mapDataToStatistics)
+            .sink { [weak self] returnedArrays in
+                self?.overviewStatistics = returnedArrays.overview
+                self?.additionalStatistics = returnedArrays.additional
             }
             .store(in: &cancellables)
+    }
+    
+    private func mapDataToStatistics(coinDetail: CoinDetail?, coin: Coin) -> (overview: [Statistic], additional: [Statistic]) {
+        
+        
+        // additional
+        
+        let overviewArray = createOverviewArray(coin: coin)
+        let additionalArray = createAdditionalArray(coinDetail: coinDetail, coin: coin)
+        
+        return (overviewArray, additionalArray)
+    
+    }
+    
+    private func createOverviewArray(coin: Coin) -> [Statistic] {
+        let price = coin.currentPrice.asCurrencyWith6Decimals()
+        let pricePercentChange = coin.priceChangePercentage24H
+        let priceStat = Statistic(title: "Current Price", value: price, percentageChange: pricePercentChange)
+        
+        let marketCap = "$" + (coin.marketCap?.formattedWithAbbreviations() ?? "")
+        let marketCapPercentChange = coin.marketCapChangePercentage24H
+        let marketCapStat = Statistic(title: "Market Capitalization", value: marketCap, percentageChange: marketCapPercentChange)
+        
+        let rank = "\(coin.rank)"
+        let rankStat = Statistic(title: "Rank", value: rank, percentageChange: nil)
+        
+        let volume = "$" + (coin.totalVolume?.formattedWithAbbreviations() ?? "")
+        let volumeStat = Statistic(title: "Volume", value: volume, percentageChange: nil)
+        
+        let overviewArray: [Statistic] = [
+            priceStat, marketCapStat, rankStat, volumeStat
+        ]
+        
+        return overviewArray
+    }
+    
+    private func createAdditionalArray(coinDetail: CoinDetail?, coin: Coin) -> [Statistic] {
+        let high = coin.high24H?.asCurrencyWith6Decimals() ?? "n/a"
+        let highStat = Statistic(title: "24h High", value: high, percentageChange: nil)
+        
+        let low = coin.low24H?.asCurrencyWith6Decimals() ?? "n/a"
+        let lowStat = Statistic(title: "24h Low", value: low, percentageChange: nil)
+        
+        let priceChange = coin.priceChange24H?.asCurrencyWith6Decimals() ?? "n/a"
+        let pricePercentChange = coin.priceChangePercentage24H
+        let priceChangeStat = Statistic(title: "24h Price Change", value: priceChange, percentageChange: pricePercentChange)
+        
+        let marketCapChange = "$" + (coin.marketCapChange24H?.formattedWithAbbreviations() ?? "")
+        let marketCapPercentChange = coin.marketCapChangePercentage24H
+        let marketCapChangeStat = Statistic(title: "24h Market Cap Change", value: marketCapChange, percentageChange: marketCapPercentChange)
+        
+        let blockTime = coinDetail?.blockTimeInMinutes ?? 0
+        let blockTimeString = blockTime == 0 ? "n/a" : "\(blockTime)"
+        let blockStat = Statistic(title: "Block Time", value: blockTimeString, percentageChange: nil)
+        
+        let hashing = coinDetail?.hashingAlgorithm ?? "n/a"
+        let hashStat = Statistic(title: "Hashing Algorithm", value: hashing, percentageChange: nil)
+        
+        let additionalArray: [Statistic] = [
+            highStat, lowStat, priceChangeStat, marketCapChangeStat, blockStat, hashStat
+        ]
+        
+        return additionalArray
     }
 }
--- Crypto/Core/Home/Detail/Views/DetailView.swift
+++ Crypto/Core/Home/Detail/Views/DetailView.swift
@@ -22,21 +22,83 @@ struct DetailLoadingView: View {
 struct DetailView: View {
     
     @StateObject var vm: DetailViewModel
-    let coin: Coin
+    
+    private let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    private let spacing: CGFloat = 30
     
     init(coin: Coin) {
-        self.coin = coin
         _vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
-        print("Initializing detai view for \(coin.name)")
     }
     
     var body: some View {
-        Text("hello")
+        ScrollView {
+            VStack(spacing: 20) {
+                Text("")
+                    .frame(height: 150)
+                
+                overviewTitle
+                Divider()
+                overviewGrid
+                additionalTitle
+                Divider()
+                additionalGrid
+            }
+            .padding()
+        }
+        .navigationTitle(vm.coin.name)
+    }
+}
+
+extension DetailView {
+    private var overviewTitle: some View {
+        Text("Overview")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var additionalTitle: some View {
+        Text("Additional Details")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var overviewGrid: some View {
+        LazyVGrid(
+            columns: columns,
+            alignment: .leading,
+            spacing: spacing,
+            pinnedViews: []) {
+                ForEach(vm.overviewStatistics) { stat in
+                    StatisticView(stat: stat)
+                }
+            }
+    }
+    
+    private var additionalGrid: some View {
+        LazyVGrid(
+            columns: columns,
+            alignment: .leading,
+            spacing: spacing,
+            pinnedViews: []) {
+                ForEach(vm.additionalStatistics) { stat in
+                    StatisticView(stat: stat)
+                }
+            }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(coin: dev.coin)
+        NavigationView {
+            DetailView(coin: dev.coin)
+        }
     }
 }
